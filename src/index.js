8e89022ca8233d267aff0c79ea4d95abbac598f9
Eva Jelinkova <evajelinkova@Pavels-Home-Pro.local>
2020-02-03 13:32:01 +0000

    first my app commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// ReactDOM.render(<div><h1>Hello world</h1><p>this is a paragraph</p></div>, document.getElementById("root"));
+
+// function MyApp() {
+//   return (
+//     <ul>
+//         <li>first</li>
+//         <li>second</li>
+//         <li>third</li>
+//     </ul>
+//     )
+//   }
+//
+// ReactDOM.render(
+//   <MyApp />,
+//     document.getElementById("root")
+//   )
