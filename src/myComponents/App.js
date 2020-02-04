891b8738e0c96e233c8634d729564fc0bd0426de
Eva Jelinkova <evajelinkova@Pavels-Home-Pro.local>
2020-02-04 20:44:08 +0000

    Q&A jokes ex.

--- src/myComponents/App.js
+++ src/myComponents/App.js
@@ -1,3 +1,7 @@
+
+// --------checkbox & textarea excercise----------
+
+
 // import React from "react"
 // import Header from "./Header.js"
 // import Checkbox from "./Checkbox.js"
@@ -20,15 +24,72 @@
 //
 // export default App
 
+// ----------------contact card components--------------
+//
+// import React from "react"
+// import ContactCard from "./ContactCard.js"
+//
+// function App() {
+//   return (
+//     <div className="contacts">
+//
+//     <ContactCard
+//         name="Mr. Whiskerson"
+//         imgUrl="http://placekitten.com/300/200/"
+//         phone="(212) 555-1234"
+//         email="mr.whoiskerson@catnap.meow"
+//     />
+//     <ContactCard
+//         name="Fluffykins"
+//         imgUrl="http://placekitten.com/400/200/"
+//         phone="(212) 555-5432"
+//         email="fluffykins@catnap.meow"
+//     />
+//     <ContactCard
+//         name="Destroyer"
+//         imgUrl="http://placekitten.com/300/300/"
+//         phone="(212) 555-5534"
+//         email="destroyer@catnap.meow"
+//     />
+//     <ContactCard
+//         name="Felix"
+//         imgUrl="http://placekitten.com/200/100/"
+//         phone="(212) 555-1266"
+//         email="felix@catnap.meow"
+//     />
+//     </div>
+//   )
+// }
+//
+// export default App
+//
+// -----------------Joke & Punchline excercise------------------
+
 import React from "react"
-import ToDoItem from "./ToDoItem.js"
+import Joke from "./Joke.js"
+
 function App() {
   return (
-    <div className="todo-list">
-        <ToDoItem />
-        <ToDoItem />
-        <ToDoItem />
-        <ToDoItem />
+    <div>
+      <Joke
+          question="What did the Buddhist ask the hot dog vendor?"
+          punchline="“Make me one with everything.”"
+      />
+      <Joke
+          question="You know why you never see elephants hiding up in trees?"
+          punchline="Because they’re really good at it. "
+      />
+      <Joke
+          question="You know why you never see elephants hiding up in trees?"
+          punchline="Because they’re really good at it. "
+      />
+      <Joke
+          punchline="Because they’re really good at it. "
+      />
+      <Joke
+          question="You know why you never see elephants hiding up in trees?"
+          punchline="Because they’re really good at it. "
+      />
     </div>
   )
 }
---
+++ src/myComponents/Joke.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Joke(props) {
+  return(
+    <div>
+      <p style={{display: !props.question && "none"}}>Question: {props.question}</p>
+      <p style={{color: !props.question && "red" }}>Answer: {props.punchline}</p>
+      <hr/>
+    </div>
+  )
+}
+
+export default Joke
