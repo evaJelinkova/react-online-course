30be1f32b350bc51f8bcba0f15f242c8a250faba
Eva Jelinkova <jelin.eva@gmail.com>
2020-02-16 16:54:02 +0000

    class components

--- src/myComponents/App.js
+++ src/myComponents/App.js
@@ -1,25 +1,88 @@
 
-import React from "react"
-import ToDoItem from "./ToDoItem"
-import todosData from "./todosData"
+// import React from "react"
+// import ToDoItem from "./ToDoItem"
+// import todosData from "./todosData"
 
-function App() {
-  const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item} />)
+// function App() {
+//   const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item} />)
 
+//   return (
+//     <div className="todo-list">
+//       {todoItems}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from "react"
+
+
+
+// class App extends React.Component {
+//   render() {
+//     const date = new Date();
+//     return (
+//     <div>
+//       <h1>Code goes here</h1>
+//     </div>
+//     )
+//   }
+// }
+
+
+// export default App
+
+
+
+
+// import React from "react"
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       answer: "yes"
+//     }
+//   }
+//   render() {
+
+//     return (
+//       <div>
+//         <h1>is state important to know? {this.state.answer}</h1>
+//         <ChildComponent answer={this.state.answer}/>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+
+import React, {Component} from "react"
+
+class App extends Component {
+  constructor() {
+  super() 
+    this.state = {
+      name: "eva",
+      age: 25
+    }
+  }
+    render() {
   return (
-    <div className="todo-list">
-      {todoItems}
-
+    <div>
+      <h1>{this.state.name}</h1>
+      <h3>{this.state.age} years old</h3>
     </div>
   )
+    }
+
 }
 
 export default App
-
-
-
-
-
 
 
 
--- src/myComponents/ToDoItem.js
+++ src/myComponents/ToDoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function ToDoItem(props) {
   return(
     <div className="todo-item">
-      <input type="checkbox" />
+      <input type="checkbox" checked={props.item.completed} />
   <p>{props.item.text}</p>
     </div>
     )
